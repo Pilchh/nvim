9c25ad0c7fb654f76124179131f9166eceb60e23
Pilchh <alex@thepilchers.com>
2025-04-17 16:37:00 +0100

    Updated telescope keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,8 +12,8 @@ vim.keymap.set('n', '<C-f>', tree_api.tree.toggle)
 
 -- Telescope Keymaps
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fa', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>ff', builtin.git_files, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
