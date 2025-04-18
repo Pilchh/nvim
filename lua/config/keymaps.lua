af03b89a5266b248f073551b317a4a6fbd378621
Pilchh <alex@thepilchers.com>
2025-04-18 01:48:05 +0100

    Added keybinds for write and quit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
+-- General keymaps
+vim.keymap.set('n', '<leader>w', '<cmd>:w!<cr>')
+vim.keymap.set('n', '<leader>q', '<cmd>:q<cr>')
+
 -- Window keymaps
 vim.keymap.set('n', '<C-s>', '<cmd>split<cr>')
 vim.keymap.set('n', '<C-v>', '<cmd>vsplit<cr>')
@@ -22,10 +26,10 @@ local harpoon = require("harpoon")
 vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end)
 vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-;>", function() harpoon:list():select(4) end)
 
 vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
