0d75733e5e4622c72f68ae53d61d15a768dac876
Pilchh <alex@thepilchers.com>
2025-04-18 18:30:08 +0100

    Updated lualine background to be transparent

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,13 +1,17 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    config = function()
-	local transparent_theme = require("lualine.themes.auto")
-	transparent_theme.normal.c.bg = "None"
-	require('lualine').setup {
-	    options = {
-		theme = transparent_theme
-	    }
-	}
-    end,
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
+	 'nvim-lualine/lualine.nvim',
+	 config = function()
+		  local transparent_theme = require("lualine.themes.auto")
+		  transparent_theme.normal.c.bg = "None"
+		  transparent_theme.insert.c.bg = "None"
+		  transparent_theme.visual.c.bg = "None"
+		  transparent_theme.replace.c.bg = "None"
+		  transparent_theme.inactive.c.bg = "None"
+		  require('lualine').setup {
+				options = {
+					 theme = transparent_theme
+				}
+		  }
+	 end,
+	 dependencies = { 'nvim-tree/nvim-web-devicons' }
 }
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,13 +1,14 @@
 return {
-	 'projekt0n/github-nvim-theme',
+	 'tim3nd/github-nvim-theme',
 	 name = 'github-theme',
+	 branch = "feat-support_blink_cmp",
 	 lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	 priority = 1000, -- make sure to load this before all the other start plugins
 	 config = function()
 		  require('github-theme').setup({
 				options = {
 					 darken = {
-						  floats = false, 
+						  floats = false,
 						  sidebars = {
 								enable = false
 						  }
